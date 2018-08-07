cd61112b4c5bf4c6b5fd0a11d68fa9da756f6c5e
wangyue <18510209803@163.com>
2018-08-07 17:45:52 +1000

    Fix a dashboard bug

--- public_html/index.php
+++ public_html/index.php
@@ -5,10 +5,9 @@
  * Date: 23/7/18
  * Time: 11:34 AM
  */
-
+require_once __DIR__ . '/../vendor/autoload.php';
 error_reporting(env('DEV_MODE',false) ? E_ERROR : 0);
 ini_set('display_errors', env('DEV_MODE',false) ? true : false);
-require_once __DIR__ . '/../vendor/autoload.php';
 
 /**
  * Route: /  -> It's the entry point of the application, will render login and 3brands grid view
