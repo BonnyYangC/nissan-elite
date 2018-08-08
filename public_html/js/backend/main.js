6bf48fd8776c86988b0cabf16edd292925ae06a3
wangyue <18510209803@163.com>
2018-08-08 11:03:39 +1000

    Refine admin form styles

--- public_html/js/backend/main.js
+++ public_html/js/backend/main.js
@@ -1,6 +1,18 @@
+function fileInputOnChange(file,fileNameElementId){
+    file.onchange = function(){
+        if(file.files.length > 0){
+            document.getElementById(fileNameElementId).innerHTML = file.files[0].name;
+        }
+    };
+}
 Zepto(function($){
     $('#submit-import-btn').on('click',function(e){
         $('#submit-btn-waiting').addClass('fa-spin');
         $('#submit-btn-txt').text('Please wait ...')
     });
-})
+
+    var csvFile = document.getElementById("csv-file-input");
+    if(csvFile){
+        fileInputOnChange(csvFile, 'csv-file-input-text');
+    }
+});
