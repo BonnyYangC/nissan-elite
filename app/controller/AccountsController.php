13aaff507ad088bbb7e8af1fd920db2bebe4edee
wangyue <justinwang24@yahoo.com.au>
2018-09-19 10:15:51 +1000

    Refine incentives page and action in controller
    Add a coming soon image in case of no current incentive

--- app/controller/AccountsController.php
+++ app/controller/AccountsController.php
@@ -79,159 +79,16 @@ class AccountsController extends DashboardController
     }
 
     /**
-     * Prepare metrics data for service manager
-     */
-    private function _prepareForRetailSalesConsultant(){
-        $role = new RetailSalesConsultant($this->userObject);
-        $this->dataForView['metrics'] = $role->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $role->name;
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/'.$role->name.'.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for service manager
-     */
-    private function _prepareForServiceAdviser(){
-        $role = new ServiceAdviser($this->userObject);
-        $this->dataForView['metrics'] = $role->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $role->name;
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/'.$role->name.'.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for service manager
-     */
-    private function _prepareForServiceManager(){
-        $role = new ServiceManager($this->userObject);
-        $this->dataForView['metrics'] = $role->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $role->name;
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/'.$role->name.'.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for parts sales rep
-     */
-    private function _prepareForPartsSalesRep(){
-        $role = new PartsSalesRep($this->userObject);
-        $this->dataForView['metrics'] = $role->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $role->name;
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/'.$role->name.'.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for Stock manager
-     */
-    private function _prepareForStockController(){
-        $role = new StockController($this->userObject);
-        $this->dataForView['metrics'] = $role->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $role->getTemplateName();
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/'.$role->getTemplateName().'.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for Sales Manager
-     */
-    private function _prepareForSalesManager(){
-        $role = new SalesManager($this->userObject);
-        $this->dataForView['metrics'] = $role->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $role->getTemplateName();
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/'.$role->getTemplateName().'.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for Parts Manager
-     */
-    private function _prepareForFleetSalesManager(){
-        $role = new FleetSalesManager($this->userObject);
-        $this->dataForView['metrics'] = $role->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $role->name;
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/'.$role->name.'.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for Parts Manager
-     */
-    private function _prepareForPartsManager(){
-        $role = new PartsManager($this->userObject);
-        $this->dataForView['metrics'] = $role->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $role->name;
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/'.$role->name.'.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for Finance Controller
-     */
-    private function _prepareForFinanceController(){
-        $financeController = new FinanceController($this->userObject);
-        $this->dataForView['metrics'] = $financeController->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $financeController->name;
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/finance_controller.js')
-        ];
-    }
-
-    /**
-     * Prepare metrics data for Finance And Insurance
-     */
-    private function _prepareForFinanceAndInsurance(){
-        $financeAndInsurance = new FI($this->userObject);
-        $this->dataForView['metrics'] = $financeAndInsurance->getMetrics($this->metricsData);
-        $this->dataForView['metrics_template_file_name'] = $financeAndInsurance->name;
-        $this->dataForView['extra_js'] = [
-            'https://www.gstatic.com/charts/loader.js',
-            asset('js/metrics/fi.js')
-        ];
-    }
-
-    /**
      * Load incentives page
      */
     public function incentives(){
         $this->dataForView['currentUri'] = 'Incentives';
         $status = strtoupper($this->request->param('status'));
-
-        $current=$thumb=$finished=$past='';
-        $incentives = Incentives::Load($status);
-        $incentivesFinished = Incentives::Load(Incentives::FINISHED);
-        $incentivesPast = Incentives::Load(Incentives::PAST);
-
         $this->dataForView['status']        = $status;
-        $this->dataForView['current']       = $current;
-        $this->dataForView['thumb']         = $thumb;
-        $this->dataForView['finished']      = $finished;
-        $this->dataForView['past']          = $past;
-
+        $this->dataForView['current']       = Incentives::GetCurrent();
+        $this->dataForView['finished']      = array_chunk(Incentives::GetJustFinished(),3);
+        $this->dataForView['past']          = array_chunk(Incentives::GetPast(),3);
         $this->dataForView['registered']            = $this->userObject->registered === 'YES';
-        $this->dataForView['incentives']            = $incentives;
-        $this->dataForView['incentivesFinished']    = array_chunk($incentivesFinished,3);
-        $this->dataForView['incentivesPast']        = array_chunk($incentivesPast,3);
-
-
         $this->dataForView['extra_css'] = [
             asset('css/fotorama.css')
         ];
