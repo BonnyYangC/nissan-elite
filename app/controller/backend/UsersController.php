d07278f8b1180ec159efa78ba8eeb68e3a7a3fdd
Justin Wang <justinwang24@yahoo.com.au>
2018-10-24 11:49:46 +1100

    update super user's password format when import

--- app/controller/backend/UsersController.php
+++ app/controller/backend/UsersController.php
@@ -166,7 +166,7 @@ class UsersController extends BaseController
                 'email'=>$row[1],
                 'firstname'=>$row[0],
                 'lastname'=>$lastName,
-                'password'=>ucfirst($lastName).'1',
+                'password'=>strtoupper(str_replace(' ','-',$lastName)).'1',
                 'position'=>'NISSAN_SUPER',
                 'active'=>1,
             ];
