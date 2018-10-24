e80701e7f36fad497b07145f4e10782f1a23a878
Justin Wang <justinwang24@yahoo.com.au>
2018-10-24 11:54:07 +1100

    u

--- app/controller/backend/UsersController.php
+++ app/controller/backend/UsersController.php
@@ -71,7 +71,7 @@ class UsersController extends BaseController
      * Load super user new view
      */
     public function super_user_new(){
-       $this->import_super_users();
+       // $this->import_super_users();
         $user = new User($this->request->param('uid'));
         $this->dataForView['user'] = $user;
         $this->render('backend/users/edit_super');
