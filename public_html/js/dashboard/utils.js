81e57d9dc3c9ba8da6e74a5ed1e0ce23a672cb5d
Kevin Drew <drewk538@gmail.com>
2019-04-08 09:54:51 +1000

    Fixed bug with dashboard barchart

--- public_html/js/dashboard/utils.js
+++ public_html/js/dashboard/utils.js
@@ -22,9 +22,9 @@ function generateGageIndicator (gageId, percent, color, text) {
     gage.appendChild(indicator);
 }
 
-for (var i = 0; i < GAGE_DATA.length; i++){
+//for (var i = 0; i < GAGE_DATA.length; i++){
     //generateGageIndicator('g1',GAGE_DATA[i][0],GAGE_DATA[i][1],GAGE_DATA[i][2]);
-}
+//}
 document.addEventListener("DOMContentLoaded", function(event) {
     var g1 = new JustGage({
         id: 'g1',
