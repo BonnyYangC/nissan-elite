cce7de7fd43d759a9b239b6954f675c9545ee399
Kevin Drew <kevin@destination.com.au>
2020-01-24 16:59:28 +1100

    legacy points lifetime points

--- app/models/utils/LifetimeUtil.php
+++ app/models/utils/LifetimeUtil.php
@@ -24,10 +24,10 @@ class LifetimeUtil
     const STATUS_LEVEL_2_COLOR      = '#C0C0C0'; //Silver
     const STATUS_LEVEL_1_COLOR        = '#8C7853'; //Bronze
 
-    const MAX_LEVEL_4 = 500000;
-    const MAX_LEVEL_3 = 325000;
-    const MAX_LEVEL_2 = 200000;
-    const MAX_LEVEL_1 = 100000;
+    const MAX_LEVEL_4 = 850000;
+    const MAX_LEVEL_3 = 500000;
+    const MAX_LEVEL_2 = 325000;
+    const MAX_LEVEL_1 = 200000;
 
     const PERCENTAGE_LEVEL_1 = (self::MAX_LEVEL_1/self::MAX_LEVEL_4)*100;
     const PERCENTAGE_LEVEL_2 = (self::MAX_LEVEL_2/self::MAX_LEVEL_4)*100;
