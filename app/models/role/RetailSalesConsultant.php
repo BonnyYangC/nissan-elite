31bb3e83d18284dd740bff1dae199b3f299e58f3
kevin <drewk538@gmail.com>
2020-04-14 15:10:40 +1000

    2020 changes to retail sales consultant

--- app/models/role/RetailSalesConsultant.php
+++ app/models/role/RetailSalesConsultant.php
@@ -46,9 +46,9 @@ class RetailSalesConsultant extends BaseRole implements IRole
         $ytd = 0;
         $dataResults = $data['Results'];
 //        dd($dataResults);
-        for($i=0; $i<11; $i++)//only show from May to Mar
+        for($i=0; $i<12; $i++)//only show from May to Mar
         {
-            $period=mktime(0,0,0,5+$i,1,$ytdParam);
+            $period=mktime(0,0,0,4+$i,1,$ytdParam);
 
             $key = $this->startPoint->addMonth()->format('M-Y');
             $item = isset($dataResults[$key]) ? $dataResults[$key] : null;
