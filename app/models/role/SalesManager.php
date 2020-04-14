cdfa08cb8769b3143152e69185654f79568f03f1
kevin <drewk538@gmail.com>
2020-04-14 15:01:32 +1000

    changed to 12 month year

--- app/models/role/SalesManager.php
+++ app/models/role/SalesManager.php
@@ -137,9 +137,9 @@ class SalesManager extends BaseRole implements IRole
         $ytd = 0;
         $dataResults = $data['Results'];
 
-        for($i=0; $i<11; $i++)//only show from May to Mar
+        for($i=0; $i<12; $i++)//only show from May to Mar
         {
-            $period=mktime(0,0,0,5+$i,1,$ytdParam);
+            $period=mktime(0,0,0,4+$i,1,$ytdParam);
             $key = $this->startPoint->addMonth()->format('M-Y');
             $item = isset($dataResults[$key]) ? $dataResults[$key] : null;
 
