9c528555367815885f63e955d8003c01ce6ca06a
kevin <drewk538@gmail.com>
2020-06-01 15:01:26 +1000

    fixed import of perf target not working

--- app/models/role/RetailSalesConsultant.php
+++ app/models/role/RetailSalesConsultant.php
@@ -28,7 +28,7 @@ class RetailSalesConsultant extends BaseRole implements IRole
         'data'=>[]
     ];
 
-    public $points_actual    =[
+    public $pointsActual    =[
         'label'=>'Perf. Target',
         'backgroundColor' => IColor::DARK_KHAKI,
         'data'=>[]
@@ -70,7 +70,7 @@ class RetailSalesConsultant extends BaseRole implements IRole
                 $this->newVehicleSales['data'][] = intval($item['credit_actual_sales']+$item['points_actual']);
                 $this->salesRecommendationSaturation['data'][]  = intval($item['salesperson_satisfaction_score']);
                 $this->followUpSaturation['data'][]  = intval($item['follow_up_satisfaction_score']);
-                $this->points_actual['data'][]  = intval($item['points_actual']);
+                $this->pointsActual['data'][]  = intval($item['points_actual']);  // Perf. Target
                 $this->keptInformed['data'][]  = intval($item['kept_informed_delivery_score']);
                 $this->trainingData['data'][]  = $item['training'] // Online
                     + $item['pathway'] + $item['training_competency'];
@@ -86,7 +86,7 @@ class RetailSalesConsultant extends BaseRole implements IRole
                 $this->newVehicleSales['data'][]  = 0;
                 $this->salesRecommendationSaturation['data'][]  = 0;
                 $this->followUpSaturation['data'][]  = 0;
-                $this->points_actual['data'][]  = 0;
+                $this->pointsActual['data'][]  = 0;
                 $this->keptInformed['data'][]  = 0;
                 $this->trainingData['data'][]  = 0;
                 $this->incentivesForDashboard['data'][] = 0;
@@ -109,7 +109,7 @@ class RetailSalesConsultant extends BaseRole implements IRole
             'metricsCurrentStatus'   =>[
                 $this->newVehicleSales,
                 $this->salesRecommendationSaturation,
-                $this->points_actual,
+                $this->pointsActual,
                 $this->keptInformed,
                 $this->followUpSaturation,
                 $this->trainingData,
