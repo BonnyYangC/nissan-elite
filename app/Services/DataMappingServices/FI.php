dfd2f5442fbf7ed5222012ca42addd256f194e6e
Bonny Yang <32720471+BonnyYangC@users.noreply.github.com>
2022-05-29 01:30:11 +1000

    metrics of FI

--- app/Services/DataMappingServices/FI.php
+++ app/Services/DataMappingServices/FI.php
@@ -14,22 +14,27 @@ class FI extends MonthlyDataMapping {
             'nfsa' => $row['points_nfsa'] !== '' ? intval($row['points_nfsa']) : 0,
             'nfsa_result' => $row['sales_nfsa_'] !== '' ? intval($row['sales_nfsa_']) : 0,
 
-            'retention' => $row['points_nfsa_LRB_'] !== '' ? intval($row['points_nfsa_LRB_']) : 0,
-            'retention_result' => $row['sales_nfsa_LRB_'] !== '' ? intval($row['sales_nfsa_LRB_']) : 0,
+            'lrb' => $row['points_nfsa_LRB_'] !== '' ? intval($row['points_nfsa_LRB_']) : 0,
+            'lrb_result' => $row['sales_nfsa_LRB_'] !== '' ? intval($row['sales_nfsa_LRB_']) : 0,
 
             'insurance_mvi' => $row['points_nfsa_MVI_'] !== '' ? intval($row['points_nfsa_MVI_']) : 0,
             'insurance_mvi_result' => $row['sales_nfsa_MVI_'] !== '' ? intval($row['sales_nfsa_MVI_']) : 0,
             'insurance_pkg' => $row['points_nfsa_PKG_'] !== '' ? intval($row['points_nfsa_PKG_']) : 0,
             'insurance_pkg_result' => $row['sales_nfsa_PKG_'] !== '' ? intval($row['sales_nfsa_PKG_']) : 0,
 
-            'emw' => $row['points_nfsa_EMW_'] !== '' ? intval($row['points_nfsa_EMW_']) : 0,
-            'emw_result' => $row['sales_nfsa_EMW_'] !== '' ? intval($row['sales_nfsa_EMW_']) : 0,
-
             'penetration' => $row['points_nfsa_PEN_'] !== '' ? intval($row['points_nfsa_PEN_']) : 0,
-            'penetration_result' => $row['pcent_nfsa_PEN_'] !== '' ? floatval($row['pcent_nfsa_PEN_']) : 0.0,
+            'penetration_result' => ($row['pcent_nfsa_PEN_'] !== '' ? number_format(floatval($row['pcent_nfsa_PEN_'])*100) : '0') . '%',
 
-            //'pmp' => $row['points_regvret_'] !== '' ? intval($row['points_regvret_']) : 0,
-            //'pmp_result' => $row['pcent_REGvRET_'] !== '' ? floatval($row['pcent_REGvRET_']) : 0.0,
+            'pmp' => $row['points_PMP'] !== '' ? intval($row['points_PMP']) : 0,
+            'pmp_result' => $row['sales_PMP'] !== '' ? intval($row['sales_PMP']) : 0,
+
+            'nfv' => $row['points_NFV'] !== '' ? intval($row['points_NFV']) : 0,
+            'nfv_result' => $row['sales_NFV'] !== '' ? intval($row['sales_NFV']) : 0,
+            'nfv_nfsa' => $row['points_NFV%'] !== '' ? intval($row['points_NFV%']) : 0,
+            'nfv_nfsa_result' => ($row['pcent_NFV%'] !== '' ? number_format(floatval($row['pcent_NFV%'])*100) : '0') . '%',
+
+            'nic_sale_nfsa' => $row['points_NIC_Fnfsa'] !== '' ? intval($row['points_NIC_Fnfsa']) : 0,
+            'nic_sale_nfsa_result' => $row['sales_NIC_Fnfsa'] !== '' ? intval($row['sales_NIC_Fnfsa']) : 0,
 
             'satisfaction' => $row['points_ce_EFI3_'] !== '' ? intval($row['points_ce_EFI3_']) : 0,
             'satisfaction_result' => $row['score_ce_EFI3_'] !== '' ? floatval($row['score_ce_EFI3_']) : 0.0,
