25c2d458e5c26b53f52f7caad3176bab87968c48
Bonny Yang <32720471+BonnyYangC@users.noreply.github.com>
2022-06-03 11:01:17 +1000

    guild button

--- resources/views/pages/guild/buttons.blade.php
+++ resources/views/pages/guild/buttons.blade.php
@@ -3,6 +3,8 @@
         <a href="{{ route('guild') }}">
             @if ($currentUri_sub=='MDguild')
                 <img class="img-responsive" src="{{ asset('images/md_guild/Guild_main_gold_button.png?a=1') }}" width="100%">
+            @elseif ($currentUri_sub=='MDguild_events')
+                <img class="img-responsive" src="{{ asset('images/md_guild/Guild_main_outline_button_white.png?a=1') }}" width="100%">
             @else
                 <img class="img-responsive" src="{{ asset('images/md_guild/Guild_main_outline_button.png?a=1') }}" width="100%">
             @endif
@@ -13,6 +15,8 @@
         <a href="{{ route('guild.members') }}">
             @if ($currentUri_sub=='MDguild_members')
                 <img class="img-responsive" src="{{ asset('images/md_guild/Guild_members_gold_button.png?a=1') }}" width="100%">
+            @elseif ($currentUri_sub=='MDguild_events')
+                <img class="img-responsive" src="{{ asset('images/md_guild/Guild_members_outline_button_white.png?a=1') }}" width="100%">
             @else
                 <img class="img-responsive" src="{{ asset('images/md_guild/Guild_members_outline_button.png?a=1') }}" width="100%">
             @endif
