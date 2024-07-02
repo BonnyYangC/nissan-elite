58f63a27d0b9a518864b26219b9fe8bd1d45b3f1
BonnyYangC <bonny.yang1981@gmail.com>
2024-07-03 08:47:51 +1000

    filter team member by year

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -93,6 +93,7 @@ class UserService extends BaseService {
             ->where('users.active', 1)
             ->where('dealers.code', $dealer)
             ->whereIn('position_code', $membersPosition)
+            ->where('territory_reports.year', config('elite.YEAR'))
             ->orderBy($parameters['sortby'], $parameters['order'])
             ->get();
     }
@@ -111,6 +112,7 @@ class UserService extends BaseService {
             ->join('dealers', 'dealers.code', '=', 'users.dealer_code')
             ->join('positions', 'positions.code', '=', 'users.position_code')
             ->join('territory_reports', 'territory_reports.employee_code', '=', 'users.employee_code')
+            ->where('territory_reports.year', config('elite.YEAR'))
             ->where('users.active', 1)
             ->where('dealers.code', $dealer)
             ->orderBy($parameters['sortby'], $parameters['order'])
