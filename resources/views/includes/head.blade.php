f7bfd78aa965bde69bf73e2de2b0e6aa1e2fde88
BonnyYangC <bonny.yang1981@gmail.com>
2025-04-10 00:02:57 +1000

    use mix to compile css files

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -9,34 +9,6 @@
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-<!-- Styles -->
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-<link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="{{ url('/css/demo.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/style.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/layout.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/header.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/welcome.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/login.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/home.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/dashboard.css?v=100&a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/ranking.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/metrics.css?v=100&a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/team_member.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/incentives.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/future_sales.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/program.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/calendar.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/product_challenge.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/awards.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/loyalty.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/guild.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/account.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/faq.css?a=3') }}" />
-<link rel="stylesheet" type="text/css" href="{{ url('/css/territory_report.css?a=3') }}" />
-
 <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
 
 <!-- jQuery 1.8 or later, 33 KB -->
@@ -49,9 +21,19 @@
 <link  href="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
 
-<script src="{{ mix('/js/app.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 <script src="{{ asset('js/dsm_utils.js') }}" defer></script>
 <script src="{{ asset('js/dashboard_utils.js') }}" defer></script>
 <script src="{{ asset('js/ranking.js') }}" defer></script>
 <script src="{{ asset('js/calendar.js') }}" defer></script>
 <script src="{{ asset('js/login.js') }}" defer></script>
+
+<!-- Styles -->
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+<link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{ url('/css/demo.css?a=3') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/css/style.css?a=3') }}" />
+
+<link rel="stylesheet" href="{{ mix('css/individual.css') }}" />
+<link rel="stylesheet" href="{{ asset('themes/' . config('view.theme') . '/css/style.css') }}">
