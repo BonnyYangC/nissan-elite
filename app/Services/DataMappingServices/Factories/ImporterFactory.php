bae176f95af8a2f19b18837b39275e3ffdf5285f
BonnyYangC <bonny.yang1981@gmail.com>
2025-05-31 20:23:27 +1000

    add factory

---
+++ app/Services/DataMappingServices/Factories/ImporterFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services\DataMappingServices\Factories;
+
+use App\Helper\Defination;
+use App\Services\DataMappingServices as DMS;
+use Illuminate\Contracts\Container\Container;
+
+class ImporterFactory
+{
+    private $container;
+    private $individualFactory;
+
+    protected $map = [
+        Defination::DATA_TYPE_USERS_INFO => DMS\ImportationImpl\User::class,
+        Defination::DATA_TYPE_DEALERS_INFO => DMS\ImportationImpl\Dealer::class,
+        Defination::DATA_TYPE_REGION_STAFF_INFO => DMS\ImportationImpl\RegionStaff::class,
+        Defination::DATA_TYPE_LOYALTY_HISTORICAL => DMS\ImportationImpl\LoyaltyHistorical::class,
+        Defination::DATA_TYPE_RANKING => DMS\ImportationImpl\Ranking::class,
+        Defination::DATA_TYPE_TERRITORY_REPORT => DMS\ImportationImpl\TerritoryReport::class
+    ];
+
+    public function __construct(Container $container, IndividualFactory $individualFactory) {
+        $this->container = $container;
+        $this->individualFactory = $individualFactory;
+      }
+
+    public function make(string $type)
+    {
+        if (!in_array($type, array_keys($this->map))) {
+          return $this->individualFactory->make($type, Defination::ACTION_TYPE_SYNC);
+        }
+        if (!isset($this->map[$type])) {
+            throw new \InvalidArgumentException("Unknown importor type");
+        }
+    
+        return $this->container->make($this->map[$type]);
+    }
+}
