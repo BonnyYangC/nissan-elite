c1cf196012cd2891f581b98427467c96576e3f33
BonnyYangC <bonny.yang1981@gmail.com>
2025-06-02 22:35:54 +1000

    update 5

--- resources/views/themes/2025/product_challenge_footer.blade.php
+++ resources/views/themes/2025/product_challenge_footer.blade.php
@@ -10,7 +10,7 @@
 </br>
 <a class="edm-link" target="_blank"
   href="{{theme_config('product_challenge_table_registe_url')}}">Click to REGISTER YOUR
-  PRODUCT CHALLENGE TABLE FOR FY24
+  PRODUCT CHALLENGE TABLE FOR {{ theme_config('FY_WITH_YEAR') }}
 </a>
 </br>
 <span style="color:#c0133c">(1 Table per Dealer with Maximum seating 10)</br>
