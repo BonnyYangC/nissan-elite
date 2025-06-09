e4faa55974507a5292db7513709fdd467f6057c2
BonnyYangC <bonny.yang1981@gmail.com>
2025-06-09 22:37:50 +1000

    modify last year url

--- config/theme/2024.php
+++ config/theme/2024.php
@@ -6,7 +6,7 @@ return array (
   'eventRegisterUrl'=>"https://www.nissanelite.com.au/reg2024",
   'FY_WITH_YEAR'=>'FY24',
   'FY_LAST_YEAR'=>'FY23',
-  'LAST_YEAR_EVENT_URL'=>"https://staging.nissanelite.com.au/elite_individual",
+  'LAST_YEAR_EVENT_URL'=>"https://fy23.nissanelite.com.au/elite_individual",
 
   'feature_further_sales' => true,
 
