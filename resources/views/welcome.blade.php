76d03f3a196842a2edda1160647b7749b911369d
BonnyYangC <bonny.yang1981@gmail.com>
2025-06-18 02:24:01 +1000

    more adjust

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,14 @@
         @include('pages.widgets.logo')
 
         <!-- Row 2: 60% height -->
-        <div class="flex-grow-0" style="height: 30%;">
+        <div class="flex-grow-0 welcome-middle-section">
             <div class="h-100 d-flex justify-content-center align-items-center">
                 <img class="nissan-elite-brand img-fluid" src="{{ theme_image('nissan/Nissan_ELITE_Main.png') }}" alt="Nissan Elite Image">    
             </div>
         </div>
 
         <!-- Row 3: 20% height -->
-        <div class="flex-grow-0" style="height: 20%;">
+        <div class="flex-grow-0 welcome-foot-section">
             <div class="h-100 container">
                 <div class="row h-100 justify-content-center">
                     {{-- Entry point for Nissan Elite Dealership --}}
@@ -31,7 +31,7 @@
                     </div>
 
                     {{-- Entry point for Nissan Elite Individual --}}
-                    <div class="col-lg-2 col-md-3 col-sm-5 col-6 d-flex align-items-center justify-content-center p-1">
+                    <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-5 col-6 d-flex align-items-center justify-content-center p-1">
                         <a href="{{ route('elite_individual') }}" class="w-100 h-100 position-relative individual-wrapper">
                             <img src="{{ theme_image('nissan/ELITE_iELITE_button.png') }}" alt="Elite Individual Button"
                                 class="img-fluid h-100 w-100 individual-img-static" style="object-fit: contain;">
