abb0691cf3ffb0f7114a038b7c5b0e85afff7119
BonnyYangC <bonny.yang1981@gmail.com>
2025-06-18 21:23:57 +1000

    final fix

--- resources/views/pages/widgets/logo.blade.php
+++ resources/views/pages/widgets/logo.blade.php
@@ -1,4 +1,4 @@
-<div class="flex-grow-0" style="height: 20%;">
+<div class="flex-grow-0 logo-section">
   <div class="h-100 d-flex header-logo align-items-center">
     <a href="{{ url('/') }}">
       <img src="{{ theme_image('nissan/Nissan_logo.png') }}" alt="Nissan Logo Image" class="img-fluid logo-img">
