49ec9cea59a453c568b9987909cfd92c3f35aafc
GekkoCreations <Adam@gekkocreations.com.au>
2025-06-19 10:58:52 +1000

    update domain and last year url

--- config/themes.php
+++ config/themes.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'nissanelite-com-au' => '2025',
+    'www-nissanelite-com-au' => '2025',
     'fy24-nissanelite-com-au' => '2024',
+    'staging-nissanelite-com-au' => '2025',
     'localhost' => '2025',
     'default' => 'default',
 ];
